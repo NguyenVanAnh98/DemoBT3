197513482e4eaf713cbe0240edecfac0d05b5fb5
NguyenVanAnh98 <nguyenanhtk321@gmail.com>
2023-11-10 14:45:11 +0700

    capnhap4

---
+++ TH_luyentapcautruc_IF_ELSE_IF/th_luyentapcautruc_if_else_if.js
@@ -0,0 +1,12 @@
+let useName = prompt("Ban la ai?",'');
+if(useName=='AnhKun'){
+    let pass = prompt('Password','');
+    if (pass =='Anhkun'){
+        alert('Welcome!','');
+    }
+}else if(useName==null) {
+    alert('Canceled');
+}else{
+        alert('Toi khong biet ban');
+
+}
